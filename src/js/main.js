941ac437ca9cdae1640c4c5a225eec9cf99e0334
shane <shanedm@protonmail.com>
2023-03-06 22:04:05 +0100

    textareas 40

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,3 @@
 // Import our custom CSS
 import '../scss/styles.scss';
 
-import { Popover } from 'bootstrap';
-
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
-[...popoverTriggerList].map((popoverTriggerEl) => new Popover(popoverTriggerEl));
