d295ca000e4d410e106097104889aa8acaf43aef
Mcandrew <Shane.Mcandrew@klm.com>
2023-03-06 18:54:45 +0100

    improve popup

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,7 @@
 // Import our custom CSS
 import '../scss/styles.scss';
 
-// Import only the Bootstrap components we need
-import { Util, Dropdown, Offcanvas, Popover } from 'bootstrap';
+import { Popover } from 'bootstrap';
 
-// Create an example popover
-document.querySelectorAll('[data-bs-toggle="popover"]')
-  .forEach(popover => {
-    new Popover(popover)
-  })
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+[...popoverTriggerList].map((popoverTriggerEl) => new Popover(popoverTriggerEl));
