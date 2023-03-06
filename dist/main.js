21406c63f3669726b0aefb7cced63dafdebe4ccc
Mcandrew <Shane.Mcandrew@klm.com>
2023-03-06 17:05:36 +0100

    fixed modules

--- dist/main.js
+++ dist/main.js
@@ -1,12 +1,6 @@
-// Import only the Bootstrap components we need
-import {
-  Util, Dropdown, Offcanvas, Popover,
-} from 'bootstrap';
+import * as bootstrap from 'bootstrap';
 
-import { createPopper } from '@popperjs/core';
-
-// Create an example popover
 document.querySelectorAll('[data-bs-toggle="popover"]')
   .forEach((popover) => {
-    new Popover(popover);
+    new bootstrap.Popover(popover);
   });
