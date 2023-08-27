f490fde45b61f4a5db79fd4e2257a92b8f9d64c3
Mcandrew <Shane.Mcandrew@klm.com>
2023-08-27 09:54:08 +0200

    move loglevel import to test.html

--- pub/test/testexampleclass.js
+++ pub/test/testexampleclass.js
@@ -1,13 +1,11 @@
 import MyClass from '../js/exampleclass.js';
-import log from '../js/libs/loglevel.js';
 
 log.enableAll();
-const { assert } = chai;
 
 describe('MyClass tests', () => {
   describe('The class', () => {
     it('can be instantiated', () => {
-      assert.isObject(new MyClass());
+      chai.assert.isObject(new MyClass());
     });
     it('a = 1', () => {
       const c = new MyClass();
