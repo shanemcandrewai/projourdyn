2d7f24c9d7c80f21c3c3393767b7f8d543e43da7
Mcandrew <Shane.Mcandrew@klm.com>
2023-09-04 19:10:46 +0200

    add access code input

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,6 +1,9 @@
 document.getElementById('dropbox').addEventListener(
   'click',
-  () => {
-    window.open('https://www.dropbox.com/oauth2/authorize?client_id=0jblm44r25b76xf&response_type=code', '_blank');
-  },
+  () => window.open('https://www.dropbox.com/oauth2/authorize?client_id=0jblm44r25b76xf&response_type=code', '_blank'),
+);
+
+document.getElementById('code').addEventListener(
+  'input',
+  () => window.open('https://www.dropbox.com/oauth2/authorize?client_id=0jblm44r25b76xf&response_type=code', '_blank'),
 );
